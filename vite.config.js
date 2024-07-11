88a5e73ef1ed421d51748683d9d5a981ec1f020e
mdsaiful1700 <saifulsylhet00@gmial.com>
2024-07-11 23:32:38 +0600

    all is well

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        news: resolve(__dirname, "about.html"),
-        news: resolve(__dirname, "service.html"),
-        news: resolve(__dirname, "blog.html"),
-        news: resolve(__dirname, "contact.html"),
-        
+        index: 'index.html',
+        about: 'about.html',
+        service: 'service.html',
+        blog:'blog.html',
+        contact:'contact.html',
       },
     },
   },
